63430e554dccf99f957f52e8499817202863d1eb
Abdalla <hyky69xu@ads.uni-magdeburg.de>
2023-04-18 13:17:01 +0200

    'gggg'

---
+++ pf.php
@@ -0,0 +1,37 @@
+<meta charset="utf-8" />
+
+<?php
+
+$db_port = '3307';
+$host= 'localhost';
+$username='root';
+$password= '';
+$DB_name= 'patchfeldplan';
+
+$conn= mysqli_connect($host, $username, $password, $DB_name, $db_port);
+
+mysqli_select_db($conn, "patchfeldplan") or die ("no database");       
+if(!$conn){
+echo mysqli_connect_error("Connection error") . mysqli_connect_error();
+
+}
+       
+        
+
+       
+
+
+
+function close_db(){
+    global $conn;
+    mysql_close($conn);
+}
+
+
+
+if (empty($_SESSION['tan']))
+{
+  $_SESSION['tan']=0;
+}
+
+?>
