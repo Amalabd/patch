4c709c430c44c82eea65902ceedd1f0202ee4137
Amal <amal.hu.abdalla@gmail.com>
2023-05-02 11:18:04 +0200

    kkk

--- patchplan.php
+++ patchplan.php
@@ -5,6 +5,18 @@ ini_set('session.cookie_httponly', true);
 
 session_start();
 include_once("pf.php");
+$user= $_SESSION['mail'];
+$class = $_SESSION ['class'];
+/*
+if(!empty(filter_input (INPUT_POST, "mail")))
+{
+ $_SESSION['mail'] = filter_input (INPUT_POST, "mail");
+} 
+*/
+if(empty($_SESSION['mail'])){
+  header("Location: log.php");
+}
+
 
 ?>
 
@@ -18,29 +30,59 @@ include_once("pf.php");
         rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
         crossorigin="anonymous">
-  <title>Your Title Here</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <title>-- Patch-Table --</title>
 </head>
 <body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  
+
+<nav class="navbar navbar-expand-lg " style= "background-color:#EBEBEB ;">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Patch-Plan</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="#">Log-in</a>
-        <a class="nav-link" href="#">Plan</a>
-      </div>
-    </div>
+    
+    <h4 class='fw-bold'>  <img src="logo.png" alt="" > URZ UNIVERSITÄTSRECHENZENTRUM</h4>
+    </a>
+  </div>
+</nav>
+<nav class="navbar navbar-expand-lg" style= "background-color:black; ">
+  <div class="container-fluid">
+    <a class="navbar-brand text-white fw-bold">Patch-Plan</a>
+    <span class=" collapse navbar-collapse text-white fw-bold"> Welcome to our offecial website</span>
+    <div class="d-flex text-white">
+      <a class="nav-link active text-white fw-bold " > <?php echo $user; ?>  </a>
+    <a class="nav-link active text-white fw-bold "  aria-current="page" href= 'log.php?session_unset()' role='button' aria-pressed='true'>Log-out</a>
+
+</div>
   </div>
 </nav>
 
+
 <!------The table---->
-<main class=" mt-5 p-5 border border-secondary rounded-3 shadow" style="background-color: rgba(255,255,255, 0.3);">
+<main class=" mt-5  p-5 " style="background-color: rgba(255,255,255, 0.3);">
   <section>
-    <h3 class="text-decoration-underline mb-5">Patch-Table</h3>
+    <h3 class=" mb-5 fw-bold" style= "color:#7A003F">Patch-Table</h3>
+  
+  <table class= "mb-3 p-3" style= "border:solid #7A003F 2px; ">
+  <thead>
+    <tr>
+      <th class= "p-2" style= "border:solid #7A003F 1px; ">Raum-Nutzer</th>
+      <th class= "p-2" style= "border:solid #7A003F 1px; ">Vlan</th>
+      <th class= "p-2" style= "border:solid #7A003F 1px;">Geräte_IP</th>
+      <th class= "p-2" style= "border:solid #7A003F 1px; ">PORT</th>
+      <th class= "p-2" style= "border:solid #7A003F 1px; ">Belegt</th>
+      <th class= "p-2" style= "border:solid #7A003F 1px;">Gepatcht</th>
+    </tr>
+  </thead>
+  <tr style= "text-align:center; ">
+      <th class= "p-2" style= "border:solid 1px; ">1</th>
+      <th class= "p-2" style= "border:solid 1px; ">2</th>
+      <th class= "p-2" style= "border:solid 1px;">3</th>
+      <th class= "p-2" style= "border:solid 1px; ">4</th>
+      <th class= "p-2" style= "border:solid 1px; ">5</th>
+      <th class= "p-2" style= "border:solid 1px;">6</th>
+    </tr>
+   
+  </table>
+
   </section>
 
 
@@ -56,7 +98,7 @@ $res= mysqli_fetch_all($qry2, MYSQLI_ASSOC);
      echo "<table class='table table-bordered table-striped'>";
 
        // Adding the first row for the horizontal headings
-       echo "<tr>";
+       echo "<tr style= 'background-color:#7A003F; color:white;'>";
        echo "<th></th>";
        // ========= Columns ======
 
@@ -87,28 +129,36 @@ if($cells === 25){
     
   }
 
-  echo "<td>";
+  
  
 $id =$rr ['patch_id'];
 
-
+echo "<td> <a href= 'ediit.php?patch_id=".$id."' role='button' aria-pressed='true' style='text-decoration:none; color:black;'>";
 
               
      
 
-          $stmt=mysqli_prepare($conn, "SELECT patch_id ,raum_nutzer ,vlan,port, belegt, gepatcht FROM patchfelder_tbl WHERE patch_id = ? ");
+          $stmt=mysqli_prepare($conn, "SELECT raum_nutzer ,vlan,geraete_ip,port, belegt, gepatcht FROM patchfelder_tbl WHERE patch_id = ? ");
           mysqli_stmt_bind_param($stmt, "i", $id);
           mysqli_stmt_execute($stmt);
-          mysqli_stmt_bind_result($stmt,$patch_id,$raum_nutzer,$vlan,$port,$belegt,$gepatcht);
+          mysqli_stmt_bind_result($stmt,$raum_nutzer,$vlan,$geraete_ip,$port,$belegt,$gepatcht);
           
           while( mysqli_stmt_fetch($stmt)){
            
-            
-echo  "<br> <span class='text-primary'>patch_id:</span> ". htmlspecialchars($patch_id). "<br>" . "raum_nutzer:". htmlspecialchars($raum_nutzer). "<br>" . "vlan : " .htmlspecialchars($vlan). 
-"<br>"."port : " .htmlspecialchars($port). "<br>".
-"belegt : " .htmlspecialchars($belegt). "<br>"."gepatcht : " .htmlspecialchars($gepatcht). "<br>"."<br>";}
+            echo ""   ;  
+echo  "<br> <span class='text-primary'></span> ". "1: ". htmlspecialchars($raum_nutzer). "<br>" . " 2 : " .htmlspecialchars($vlan).
+"<br>" . "3 : " .htmlspecialchars($geraete_ip).
+"<br>"."4 : " .htmlspecialchars($port). "<br>".
+"5 : " .(($belegt == 0)? '<span class="text-danger"><i class="fa fa-plug" aria-hidden="true"></i>
+</span>' : '<span class="text-success"><i class="fa fa-plug" aria-hidden="true"></i>
+</span>').
+ "<br>"."6 : " .(($gepatcht  == 0) ? '<span class="text-danger"><i class="fa fa-circle" aria-hidden="true"></i>
+ </span>' : '<span class="text-success"><i class="fa fa-circle" aria-hidden="true"></i>
+ </span>'). "<br>"."<br>";
+ echo  "";
+}
 
-          echo "</td>";
+          echo "</a></td>";
          }
 
        
